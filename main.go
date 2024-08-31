b288057785d0cce3803a854a08525a2ac9d36818
RaihanAnthony <raihanmalay14@gmail.com>
2024-08-31 17:08:45 +0700

    updated cors origin

--- main.go
+++ main.go
@@ -14,17 +14,10 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	r.Use(handlers.CORS(
-		handlers.AllowedOrigins([]string{"*"}),
-		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
-		handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
-		handlers.AllowCredentials(),
-	))
-
+	r.Use(corsMiddlewares)
 	config.DB_Connection()
 	
-	r.HandleFunc("/berkahjaya/get/hadiah", controller.Hadiah).Methods("GET", "OPTIONS")
-	
+	r.HandleFunc("/berkahjaya/get/hadiah", controller.Hadiah).Methods("GET")
 	api := r.PathPrefix("/berkahjaya").Subrouter()
 	api.Use(middlewares.JWTMiddleware)
 	api.HandleFunc("/gifts/have/change/user", controller.GiftHasExchanged).Methods("GET")
@@ -37,4 +30,27 @@ func main() {
 	api.HandleFunc("/change/password", controller.ChangePassword).Methods("POST")
 	
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func corsMiddlewares(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		origin := r.Header.Get("Origin")
+
+		allowedOrigins := map[string]bool{
+			"https://fe-tb-berkah-jaya-750892348569.us-central1.run.app" : true,
+		}
+
+		if allowedOrigins[origin] {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			W.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization")
+			W.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
+
+		if r.Method == http.MethodOptions {
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
